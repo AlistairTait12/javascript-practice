3b336fdb74c2dfed9631c316277560180a4af65d
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-04-26 19:39:36 +0100

    created basic html and css. Templated guide for script

---
+++ tags-box/script.js
@@ -0,0 +1,12 @@
+// Get the elements to be worked with
+let removeButtons = Array.from(document.querySelectorAll('.removeButton'));
+
+// Add event listener to 'add' button
+// We may also need to add event listeners to all 'removeButtons'
+// AND update the let removeButtons variable
+
+// Function() that will put the tag in the box
+// Should there be a helper method to create and return a tag element?
+// Maybe even a js class?
+
+// Function() that will remove a tag from a box if said tag's removeButton is clicked
