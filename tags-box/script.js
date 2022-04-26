5aef8252e3c17240251f0af8539a07bfd89fbfd8
Alistair Tait <Alistair.Tait@bglgroup.co.uk>
2022-04-26 23:24:30 +0100

    tags-box: added some javascript to determine what to add into the box when user enters text into the input field

--- tags-box/script.js
+++ tags-box/script.js
@@ -1,11 +1,28 @@
 // Get the elements to be worked with
 let removeButtons = Array.from(document.querySelectorAll('.removeButton'));
 
+let tagsContainer = document.getElementById('tagsContainer')
+
 // Add event listener to 'add' button
 // We may also need to add event listeners to all 'removeButtons'
 // AND update the let removeButtons variable
 
 // Function() that will put the tag in the box
+function addTagToContainer(textString) {
+  let button = document.createElement('button');
+  button.classList.add('removeButton');
+  button.innerHTML = 'x';
+
+  let span = document.createElement('span');
+  span.classList.add('tagElement');
+
+  span.innerHTML = `#${textString} `;
+
+  span.appendChild(button);
+
+  tagsContainer.appendChild(span);
+}
+
 // Should there be a helper method to create and return a tag element?
 // Maybe even a js class?
 
